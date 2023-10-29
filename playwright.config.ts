38dba8d86b4f52491c070079bb3bf09013592334
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2023-10-29 14:18:11 +0100

    Use a UserAgent that will get filtered out of analytics as a bot.

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    userAgent: `BtYR-E2E-Bot ${version}`,
+    userAgent: `btyr-end-to-end-tests-v${version.replace(/\./g, "_")}-bot`,
   },
 
   /* Configure projects for major browsers */
