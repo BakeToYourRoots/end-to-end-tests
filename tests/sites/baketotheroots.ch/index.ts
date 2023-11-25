42dd74ee10eb506ea3d9f6570b74f39e29f0157c
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2023-11-25 08:04:25 +0100

    feat(test): Add check for Sitemap presence in robots.txt

--- tests/sites/baketotheroots.ch/index.ts
+++ tests/sites/baketotheroots.ch/index.ts
@@ -1,9 +1,10 @@
 import { test as base } from "@playwright/test";
-import { AboutPage, IndexPage } from "./pages";
+import { AboutPage, IndexPage, RobotsTxt } from "./pages";
 
 type Fixtures = {
   aboutPage: AboutPage;
   indexPage: IndexPage;
+  robotsTxt: RobotsTxt;
 };
 
 export const test = base.extend<Fixtures>({
@@ -17,6 +18,35 @@ export const test = base.extend<Fixtures>({
     await p.goto();
     await use(p);
   },
+  robotsTxt: async ({ page }, use) => {
+    const p = new RobotsTxt(page);
+    await p.goto();
+    await use(p);
+  },
 });
 
-export { expect } from "@playwright/test";
+import { expect as baseExpectations } from "@playwright/test";
+import robotsParser from "robots-parser";
+
+export const expect = baseExpectations.extend({
+  async toReferenceSitemaps(robotsTxt: RobotsTxt) {
+    const fileContents = await robotsTxt.page.content();
+
+    const parsedRobotsTxt = robotsParser(process.env.BASE_URL!, fileContents);
+    const numberOfSitemaps = parsedRobotsTxt.getSitemaps().length;
+
+    const pass = numberOfSitemaps > 0;
+
+    const message = pass
+      ? () => `robots.txt references ${numberOfSitemaps} sitemaps.`
+      : () => "robots.txt does not reference any sitemaps.";
+
+    return {
+      message,
+      name: "toReferenceSitemaps",
+      expected: "> 0",
+      actual: numberOfSitemaps,
+      pass,
+    };
+  },
+});
