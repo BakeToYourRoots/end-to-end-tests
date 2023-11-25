3da58f75ffb89ee5b62edb3a499dd18fed835b2f
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2023-11-25 05:52:57 +0100

    feat(test): Test newsletter form.

--- tests/sites/baketotheroots.ch/index.ts
+++ tests/sites/baketotheroots.ch/index.ts
@@ -1,11 +1,17 @@
 import { test as base } from "@playwright/test";
-import { IndexPage } from "./pages/IndexPage";
+import { AboutPage, IndexPage } from "./pages";
 
 type Fixtures = {
+  aboutPage: AboutPage;
   indexPage: IndexPage;
 };
 
 export const test = base.extend<Fixtures>({
+  aboutPage: async ({ page }, use) => {
+    const p = new AboutPage(page);
+    await p.goto();
+    await use(p);
+  },
   indexPage: async ({ page }, use) => {
     const p = new IndexPage(page);
     await p.goto();
