8c2f3b9104eb68b119aa8223967fa6efffe6463f
ItsJustRuby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2023-11-27 12:37:42 +0100

    feat: Assert 0 console errors automatically.

--- tests/sites/baketotheroots.ch/index.ts
+++ tests/sites/baketotheroots.ch/index.ts
@@ -1,7 +1,10 @@
-import { test as base } from "@playwright/test";
+import { ConsoleMessage, Page, TestInfo, test as base } from "@playwright/test";
 import { AboutPage, IndexPage, RobotsTxt } from "./pages";
 
 type Fixtures = {
+  autoNoErrors: any;
+  consoleFixture: ConsoleFixture;
+
   aboutPage: AboutPage;
   indexPage: IndexPage;
   robotsTxt: RobotsTxt;
@@ -23,12 +26,56 @@ export const test = base.extend<Fixtures>({
     await p.goto();
     await use(p);
   },
+  consoleFixture: async ({ page }, use) => {
+    const c = new ConsoleFixture(page);
+    await use(c);
+  },
+
+  autoNoErrors: [
+    async ({ consoleFixture }: Fixtures, use: any, _: TestInfo) => {
+      await use(consoleFixture);
+      expect(consoleFixture).toHaveNoMessagesOfType("error");
+    },
+    { auto: true },
+  ],
 });
 
 import { expect as baseExpectations } from "@playwright/test";
 import robotsParser from "robots-parser";
 
+class ConsoleFixture {
+  private readonly messages: ConsoleMessage[] = [];
+  constructor(page: Page) {
+    page.on("console", (consoleMessage: ConsoleMessage) => {
+      this.messages.push(consoleMessage);
+    });
+  }
+
+  public getMessagesOfType(type: string): ConsoleMessage[] {
+    return this.messages.filter((m) => m.type() == type);
+  }
+}
+
 export const expect = baseExpectations.extend({
+  async toHaveNoMessagesOfType(console: ConsoleFixture, type: string) {
+    const relevantMessages = console.getMessagesOfType(type);
+
+    const pass = relevantMessages.length == 0;
+
+    const message = pass
+      ? () => `Console contains no entries of type "${type}"`
+      : () =>
+          `Console contains ${relevantMessages.length} entries of type "${type}"`;
+
+    return {
+      message,
+      name: "toHaveNoMessagesOfType",
+      expected: 0,
+      actual: relevantMessages.length,
+      pass,
+    };
+  },
+
   async toReferenceSitemaps(robotsTxt: RobotsTxt) {
     const fileContents = await robotsTxt.page.content();
 
